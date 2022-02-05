642de8fc12c1e65f7aeddbb53411048b21d5c631
Masho Ado <mashoado@gmail.com>
2022-02-05 16:09:38 +0300

    search component for tablet and desktop

--- src/Components/SearchTablet.js
+++ src/Components/SearchTablet.js
@@ -1,93 +1,61 @@
 import React, { useState } from "react";
 
-import "../styles/Search.css";
+import "../styles/SearchTablet.css";
 
 //icons svg
-import { ReactComponent as FilterIcon } from "../assets/mobile/icon-filter.svg";
-import { ReactComponent as SearchIcon } from "../assets/desktop/icon-search-white.svg";
 import { ReactComponent as SearchIconTablet } from "../assets/desktop/icon-search.svg";
 import { ReactComponent as LocationIcon } from "../assets/desktop/icon-location.svg";
 
 function Search({ isDark }) {
-	const [show, setShow] = useState(false);
-
-	// const toggleFilter = () => {
-	// 	setShow(!show);
-	// };
-
-	const removeShow = () => {
-		setShow(false);
-	};
-
 	return (
-		<div className="search-container">
-			<div
-				className={show ? "overlay active" : "overlay"}
-				onClick={removeShow}
-			></div>
+		<div
+			className={
+				isDark
+					? "search-tablet-container search-tablet-container-dark "
+					: "search-tablet-container"
+			}
+		>
 			<div
 				className={
-					isDark
-						? "search-wrapper search-wrapper-dark"
-						: "search-wrapper search-wrapper-light"
+					isDark ? "search-tablet search-tablet-dark" : "search-tablet"
 				}
 			>
+				<SearchIconTablet />
 				<input
-					className={
-						isDark ? "search-input input-dark" : "search-input input-light"
-					}
+					className={isDark ? "input input-dark" : "input"}
 					type="text"
 					name="title"
-					placeholder="Filter by title..."
+					placeholder="Filter by title, companies, expertise..."
 				/>
-
-				<div>
-					<SearchIcon className="search-icon" />
-				</div>
 			</div>
 
 			<div
 				className={
-					!show ? "filter-container" : "filter-container show-container"
+					isDark ? "location-tablet location-tablet-dark" : "location-tablet"
 				}
 			>
-				<div
-					className={
-						isDark ? "filter-wrapper filter-wrapper-dark" : "filter-wrapper"
-					}
-				>
-					<div
-						className={
-							isDark
-								? "location-wrapper location-wrapper-dark"
-								: "location-wrapper"
-						}
-					>
-						<LocationIcon />
-						<input
-							className={
-								isDark
-									? "location-input input-dark"
-									: "location-input input-light"
-							}
-							type="text"
-							name="location"
-							placeholder="Filter by location..."
-						/>
-					</div>
+				<LocationIcon />
+				<input
+					className={isDark ? "input input-dark" : "input"}
+					type="text"
+					name="title"
+					placeholder="Filter by Location..."
+				/>
+			</div>
 
-					<div className="cotract-checkbox">
-						<input
-							className="checkbox"
-							type="checkbox"
-							name="contract"
-							// value="full time only"
-						/>
-						<label className="fulltime">Full Time Only</label>
-					</div>
+			<div
+				className={
+					isDark
+						? "checkbox-search-btn checkbox-search-btn-dark"
+						: "checkbox-search-btn"
+				}
+			>
+				<input type="checkbox" name="contract" />
+				<label className="full-time-label">
+					Full time <span className="only">Only</span>
+				</label>
 
-					<button className="search-button">Search</button>
-				</div>
+				<button className="search-btn">Search</button>
 			</div>
 		</div>
 	);
