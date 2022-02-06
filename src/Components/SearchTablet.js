0444167dd2e84ba4feabb3bc3789d8db05d970de
Masho Ado <mashoado@gmail.com>
2022-02-06 13:18:15 +0300

    set the button to trigger handleclick functions and on change functions which are imported through props

--- src/Components/SearchTablet.js
+++ src/Components/SearchTablet.js
@@ -6,7 +6,7 @@ import "../styles/SearchTablet.css";
 import { ReactComponent as SearchIconTablet } from "../assets/desktop/icon-search.svg";
 import { ReactComponent as LocationIcon } from "../assets/desktop/icon-location.svg";
 
-function Search({ isDark }) {
+function Search({ isDark, handleClick, handleChange }) {
 	return (
 		<div
 			className={
@@ -20,12 +20,13 @@ function Search({ isDark }) {
 					isDark ? "search-tablet search-tablet-dark" : "search-tablet"
 				}
 			>
-				<SearchIconTablet />
+				<SearchIconTablet onClick={handleClick} />
 				<input
 					className={isDark ? "input input-dark" : "input"}
 					type="text"
 					name="title"
 					placeholder="Filter by title, companies, expertise..."
+					onChange={handleChange}
 				/>
 			</div>
 
@@ -34,12 +35,13 @@ function Search({ isDark }) {
 					isDark ? "location-tablet location-tablet-dark" : "location-tablet"
 				}
 			>
-				<LocationIcon />
+				<LocationIcon onClick={handleClick} />
 				<input
 					className={isDark ? "input input-dark" : "input"}
 					type="text"
 					name="title"
 					placeholder="Filter by Location..."
+					onChange={handleChange}
 				/>
 			</div>
 
@@ -50,12 +52,19 @@ function Search({ isDark }) {
 						: "checkbox-search-btn"
 				}
 			>
-				<input type="checkbox" name="contract" />
+				<input
+					type="checkbox"
+					name="contract"
+					onChange={handleChange}
+					value="full time"
+				/>
 				<label className="full-time-label">
 					Full time <span className="only">Only</span>
 				</label>
 
-				<button className="search-btn">Search</button>
+				<button className="search-btn" onClick={handleClick}>
+					Search
+				</button>
 			</div>
 		</div>
 	);
