dcffea9b401ae1c8dae15e0b534afc0d712e64d6
Masho Ado <mashoado@gmail.com>
2022-02-06 13:12:48 +0300

    dynamically add data for each job item using map function

--- src/Components/JobsContainer.js
+++ src/Components/JobsContainer.js
@@ -1,26 +1,74 @@
-import React from "react";
+import React, { useContext } from "react";
 
 // styles
 import "../styles/JobsContainer.css";
-// data
-import data from "../data.json";
+import { isDarkContext } from "../App";
 
-// icons
+// import JobCard from "./JobCard";
 import { ReactComponent as Scoot } from "../assets/logos/scoot.svg";
-import JobCard from "./JobCard";
+import { ReactComponent as Blogr } from "../assets/logos/blogr.svg";
+import { ReactComponent as Coffeeroasters } from "../assets/logos/coffeeroasters.svg";
+import { ReactComponent as Creative } from "../assets/logos/creative.svg";
+import { ReactComponent as Crowdfund } from "../assets/logos/crowdfund.svg";
+import { ReactComponent as Maker } from "../assets/logos/maker.svg";
+import { ReactComponent as Mastercraft } from "../assets/logos/mastercraft.svg";
+import { ReactComponent as Officelite } from "../assets/logos/officelite.svg";
+import { ReactComponent as Pod } from "../assets/logos/pod.svg";
+import { ReactComponent as Pomodoro } from "../assets/logos/pomodoro.svg";
+import { ReactComponent as Typemaster } from "../assets/logos/typemaster.svg";
+import { ReactComponent as Vector } from "../assets/logos/vector.svg";
 
-function JobsContainer() {
-	console.log(data);
+function JobsContainer({ data }) {
+	const logos = [
+		Scoot,
+		Blogr,
+		Vector,
+		Officelite,
+		Pod,
+		Creative,
+		Pomodoro,
+		Maker,
+		Coffeeroasters,
+		Mastercraft,
+		Crowdfund,
+		Typemaster,
+		Crowdfund,
+		Coffeeroasters,
+		Blogr,
+	];
+	const isDark = useContext(isDarkContext);
 
-	const jobOne = data[0];
 	return (
 		<div className="jobs-container">
-			<JobCard />
-			<JobCard />
-			<JobCard />
-			<JobCard />
-			<JobCard />
-			<JobCard />
+			{data.map((jobItem) => {
+				const UniqueLogo = logos[jobItem.id - 1];
+
+				return (
+					<article
+						className={isDark ? "job-card job-card-dark" : "job-card"}
+						key={jobItem.id}
+					>
+						<div
+							className="job-card-icon"
+							style={{ backgroundColor: jobItem.logoBackground }}
+						>
+							<UniqueLogo />
+						</div>
+
+						<div>
+							<div className="time-contract">
+								<small>{jobItem.postedAt}</small>
+								<small className="contract">&#8226; {jobItem.contract}</small>
+							</div>
+							<div className="job-title-wrapper">
+								<span className="job-title">{jobItem.position}</span>
+							</div>
+							<small className="company-name">{jobItem.company}</small> <br />
+							<div className="country"> {jobItem.location} </div>
+						</div>
+					</article>
+				);
+			})}
 		</div>
 	);
 }
