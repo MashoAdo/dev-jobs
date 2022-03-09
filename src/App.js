8347c47efbd2916765010a820d104999d4af91ad
Masho Ado <mashoado@gmail.com>
2022-03-09 21:50:44 +0300

    add filter functionality and display filtered data when search button is clicked

--- src/App.js
+++ src/App.js
@@ -18,14 +18,46 @@ function App() {
 		setIsDark(!isDark);
 	};
 
+	// stores search term
+	const [searchTerm, setSearchTerm] = useState("");
+	const [uiData, setUiData] = useState(data);
+
+	const handleChange = (e) => {
+		// get input value and convert to lowercase , necessary to comparison when filtering
+		var search = e.target.value.toLowerCase();
+		setSearchTerm(search);
+	};
+
+	// filters data using the search term and set the data to be mapped to UI
+	const handleClick = () => {
+		if (searchTerm === "") {
+			setUiData(data);
+		}
+		console.log(searchTerm);
+		const filteredData = data.filter((item) => {
+			return (
+				// convert object values to lowercase for comparison with searchTerm
+				item.company.toLowerCase().includes(searchTerm) ||
+				item.contract.toLowerCase().includes(searchTerm) ||
+				item.location.toLowerCase().includes(searchTerm) ||
+				item.position.toLowerCase().includes(searchTerm)
+			);
+		});
+
+		setUiData(filteredData);
+	};
+
 	return (
 		<div className={isDark ? "app dark" : "app"}>
 			<Header isDark={isDark} handleSetMode={setMode} />
 			<Search isDark={isDark} />
-			<SearchTablet isDark={isDark} />
-
+			<SearchTablet
+				isDark={isDark}
+				handleChange={handleChange}
+				handleClick={handleClick}
+			/>
 			<isDarkContext.Provider value={isDark}>
-				<JobsContainer data={data} />
+				<JobsContainer data={uiData} />
 			</isDarkContext.Provider>
 			<LearnBtn />
 		</div>
